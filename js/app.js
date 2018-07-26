793d579c89725618da806e0dd4f3b1495b683c1e
Shubhankardubey <dubeyshubhankar007@gmail.com>
2018-07-26 12:47:11 +0530

    sppech done

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 function wiki() {
     this.city = 'bikaner';
+
     // this.wikipedia_url = 'https://en.wikipedia.org/wiki/' + this.city;
     // console.log(this.wikipedia_url);
     // document.getElementById("html_wiki").href = this.wikipedia_url;
@@ -17,10 +18,10 @@ function wiki() {
         dataType: 'jsonp',
         success: function (data) {
 
-console.log(data)
+//console.log(data)
              var markup = data.parse.text["*"];
              var i = $('<div></div>').html(markup);
-            $('#article').html(i);
+         //   $('#article').html(i);
 
             //remove links as they will not work
 
@@ -32,14 +33,30 @@ console.log(data)
             i.find('sup').remove();
 
             i.find('img').remove();
+            i.find('span').remove();
             // remove cite error
             i.find('.mw-ext-cite-error').remove();
 
-            $('#article').html($(i).find('p'));
+            let z = $(i).find('p');
+            //console.log($(i).find('p')[1]);
+            let arr = []
+            for(let i =1; i<z.length; i++){
+                let y = jQuery(z[i]).text();
+                arr.push(y)
+            }
+            console.log(arr)
+            for(let j=0; j<arr.length; j++){
+                var msg = new SpeechSynthesisUtterance(arr[j]);
+                window.speechSynthesis.speak(msg);
+            }
 
+           // let y = jQuery($(i).find('p')[1]).text();
+           //  console.log(y)
 
         }
     })
-
 }
+
+//console.log(document.getElementById('article').value)
+
 
